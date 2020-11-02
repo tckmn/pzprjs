155dd1cfff9c2cfbb5d1e0fbeafae9a9daa71895
Robert Vollmert <rob@vllmrt.net>
2020-11-03 00:16:53 +0100

    format

--- src-ui/sw.js
+++ src-ui/sw.js
@@ -1,26 +1,23 @@
-var CACHE_NAME = 'v1';
-var urlsToCache = [
-  '/p'
-];
+var CACHE_NAME = "v1";
+var urlsToCache = ["/p"];
 
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener("install", function(event) {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then(function(cache) {
+			return cache.addAll(urlsToCache);
+		})
+	);
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request, {ignoreSearch: true})
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+self.addEventListener("fetch", function(event) {
+	event.respondWith(
+		caches
+			.match(event.request, { ignoreSearch: true })
+			.then(function(response) {
+				if (response) {
+					return response;
+				}
+				return fetch(event.request);
+			})
+	);
 });
