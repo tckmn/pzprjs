0b40cf2dbf6b940d6447b8816a82239750b7edda
Robert Vollmert <rob@vllmrt.net>
2020-11-02 19:45:02 +0100

    Add service worker that caches /p{,.html} regardless of query parameters

---
+++ src-ui/sw.js
@@ -0,0 +1,27 @@
+var CACHE_NAME = 'v1';
+var urlsToCache = [
+  '/p.html',
+  '/p'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch: true})
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
