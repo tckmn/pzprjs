55e27034182b76f8a97788195073ebe57820f1c0
Andy Tockman <andy@tck.mn>
2022-07-11 00:35:31 -0400

    add solve counts, diffs, "open all" buttons

--- src-ui/db-extra.js
+++ src-ui/db-extra.js
@@ -1,5 +1,7 @@
 // modified re-add direct link functionality by phenomist
 
+var added = 0;
+
 setInterval(function() {
     var puzzles = document.getElementsByClassName("pzvpuzzle");
     for (var i = 0; i < puzzles.length; i++){
@@ -9,8 +11,31 @@ setInterval(function() {
             var link = src.replace("v?thumb&","?");
             var pzvlink = link.replace("https://puzz.link","http://pzv.jp");
             var pzpluslink = link.replace("https://puzz.link","");
+            var diff = puzzle.getElementsByClassName('puzzletype')[0].title;
             puzzle.getElementsByTagName("a")[0].href = pzpluslink;
-            puzzle.insertAdjacentHTML('beforeend', '<div class="plink"><a target="_blank" href="'+pzpluslink+'">[pzplus]</a> <a target="_blank" href="'+link+'">[puzz.link]</a> <a target="_blank" href="'+pzvlink+'">[pzv.jp]</a></div>');
+            puzzle.insertAdjacentHTML('beforeend', '<div class="plink"><a target="_blank" href="'+pzpluslink+'" class="lpzp">[pzplus]</a> <a target="_blank" href="'+link+'" class="lpl">[puzz.link]</a> <a target="_blank" href="'+pzvlink+'" class="lpzv">[pzv.jp]</a> <span style="padding-left:1rem">'+diff+'</span></div>');
+        }
+    }
+
+    if (!added) {
+        var paging = document.getElementsByClassName('paging')[0];
+        if (paging) {
+            added = 1;
+            var txt = document.createElement('span');
+            txt.style.marginLeft = '1rem';
+            txt.innerText = 'open all:';
+            paging.appendChild(txt);
+            [['pzplus', 'lpzp'], ['puzz.link', 'lpl'], ['pzv.jp', 'lpzv']].forEach(function(site) {
+                var btn = document.createElement('a');
+                btn.innerText = '[' + site[0] + ']';
+                btn.href = '#';
+                btn.addEventListener('click', function(e) {
+                    e.preventDefault();
+                    Array.from(document.getElementsByClassName(site[1])).forEach(function(x) { x.click(); });
+                });
+                paging.appendChild(document.createTextNode(' '));
+                paging.appendChild(btn);
+            });
         }
     }
 }, 1000);
